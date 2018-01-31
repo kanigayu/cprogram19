f8c6faabaee0e401990cf6c83dd816e8f6adeb5c
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-01-31 16:57:24 +0530

    multiply.c
    multiply

---
+++ multiply.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int k,num1,num2,num3,num4,num5;
+printf("Enter the number");
+scanf("%d",&k);
+num1=k*1;
+num2=k*2;
+num3=k*3;
+num4=k*4;
+num5=k*5;
+printf("The multiples of num is %d %d %d %d %d",num1,num2,num3,num4,num5);
+return 0;
+}
+
